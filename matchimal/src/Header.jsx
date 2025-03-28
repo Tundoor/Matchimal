874e2054b5b8c078bf202db3c0525f22f3e29c4b
Thando <thandwem045@gmail.com>
2025-03-28 09:15:16 +0200

    Created React app

---
+++ matchimal/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+
+    return (
+        <>
+            <header class="header" id="header">
+                <div class="logo" id="logo">
+                    <img src="" alt="Matchimal Logo" id="logo-img" />
+                    <h1 id="logo">Matchimal</h1>
+                </div>
+                <nav id="nav-bar">
+                    <a id="nav-link" href=""></a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
